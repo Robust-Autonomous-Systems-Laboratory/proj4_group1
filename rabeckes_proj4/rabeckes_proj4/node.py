d7d5b92a5ebe3a662c7ce3e2d002373f65190363
Reid <rabeckes@mtu.edu>
2026-02-15 14:25:40 -0500

    Base node implementation done

--- rabeckes_proj4/rabeckes_proj4/node.py
+++ rabeckes_proj4/rabeckes_proj4/node.py
@@ -1,5 +1,5 @@
 import rclpy
-#import numpy as np
+import numpy as np
 from rclpy.node import Node
 from sensor_msgs.msg import JointState, Imu
 from geometry_msgs.msg import TwistStamped
@@ -8,6 +8,8 @@ from nav_msgs.msg import Path, Odometry
 class KFNode(Node):
     def __init__(self):
         super().__init__('kf_node')
+        # Inits
+        self.filters = ['kf', 'ekf', 'ukf']
         # Create Subscribers
         self.joint_state_sub = self.create_subscription(
             JointState,
@@ -25,30 +27,18 @@ class KFNode(Node):
             self.cmdVelSub,
             10)
         # Create Publishers (could be better as a dict)
-        self.kf_path_pub = self.create_publisher(
-            Path,
-            '/localization_node/kf/path',
-            20)
-        self.ekf_path_pub = self.create_publisher(
-            Path,
-            '/localization_node/ekf/path',
-            20)
-        self.ukf_path_pub = self.create_publisher(
-            Path,
-            '/localization_node/ukf/path',
-            20)
-        self.kf_odom_pub = self.create_publisher(
-            Odometry,
-            'localization_node/kf/odometry',
-            20)
-        self.ekf_odom_pub = self.create_publisher(
-            Odometry,
-            'localization_node/ekf/odometry',
-            20)
-        self.ukf_odom_pub = self.create_publisher(
-            Odometry,
-            'localization_node/ukf/odometry',
-            20)
+        self.pubs = {}
+        for f in self.filters:
+            self.pubs[f] = {
+                'path' : self.create_publisher(
+                    Path,
+                    f'/localization_node/{f}/path',
+                    20),
+                'odom' : self.create_publisher(
+                    Odometry,
+                    f'/localization_node/{f}/odometry',
+                    20)
+            }
         # Timer callback
         self.timer = self.create_timer(0.05, self.step)
         self.i = 0
@@ -65,21 +55,32 @@ class KFNode(Node):
         return
 
     def imuSub(self, msg):
-        '''
         x = msg.orientation.x
         y = msg.orientation.y
         z = msg.orientation.z
         w = msg.orientation.w
-        yaw = np.atan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z))
+        yaw = np.arctan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z))
         self.z['theta'] = yaw
         self.get_logger().info(f'IMU: theta = {self.z['theta']}')
-        '''
         return
 
     def cmdVelSub(self, msg):
         return
 
     def step(self):
+        for f in self.filters:
+            path_msg = Path()
+            odom_msg = Odometry()
+            # Headers
+            path_msg.header.stamp = self.get_clock().now().to_msg()
+            path_msg.header.frame_id = 'odom'
+            odom_msg.header.stamp = self.get_clock().now().to_msg()
+            odom_msg.header.frame_id = 'odom'
+            # Data
+
+            # Publish
+            self.pubs[f]['path'].publish(path_msg)
+            self.pubs[f]['odom'].publish(odom_msg)
         return
 
 def main():

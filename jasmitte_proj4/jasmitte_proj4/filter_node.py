e3550e738f98a51832ee023ffbeb13807cc0c57c
Anders Smitterberg <jasmitte@mtu.edu>
2026-02-15 11:32:59 -0500

    package setup and launch file created. License and description populated.

--- jasmitte_proj4/jasmitte_proj4/filter_node.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from jasmitte_proj4.')
-
-
-if __name__ == '__main__':
-    main()
--- jasmitte_proj4/setup.py
+++ jasmitte_proj4/setup.py
@@ -10,12 +10,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/localization.launch.py']),
+        ('share/' + package_name + '/rviz', ['rviz/proj4.rviz']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='anders',
     maintainer_email='jasmitte@mtu.edu',
-    description='TODO: Package description',
+    description='A package for performing localization using KF, EKF, and UKF on a differential drive robot.',
     license='MIT',
     extras_require={
         'test': [
@@ -24,7 +26,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'filter_node = jasmitte_proj4.filter_node:main'
+            'localization_node = jasmitte_proj4.localization_node:main'
         ],
     },
 )
